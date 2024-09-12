a2944f2e2ef8b543ba8aa6f6a1e0dbde53f2cf0b
benedettoB <benedettobando@outlook.com>
2024-09-12 14:17:41 -0700

    extension functions

--- app/src/main/java/org/benedetto/catsapp/ui/App.kt
+++ app/src/main/java/org/benedetto/catsapp/ui/App.kt
@@ -1,10 +1,7 @@
 package org.benedetto.catsapp.ui
 
 import android.app.Application
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.components.SingletonComponent
 
 @HiltAndroidApp
 class App: Application()
