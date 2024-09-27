0f82b4aea91543a99920a0cf0b0ae9581051b87d
benedettoB <benedettobando@outlook.com>
2024-09-26 22:36:32 -0700

    add buffer to flow

--- app/src/main/java/org/benedetto/catsapp/ui/viewmodel/CatViewModel.kt
+++ app/src/main/java/org/benedetto/catsapp/ui/viewmodel/CatViewModel.kt
@@ -6,12 +6,14 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import org.benedetto.data.model.Cat
 import org.benedetto.data.repository.remote.CatRepository
 import org.benedetto.data.util.log
 import javax.inject.Inject
+import kotlin.system.measureTimeMillis
 
 @HiltViewModel
 class CatViewModel @Inject constructor(private val catRepository: CatRepository) : ViewModel() {
@@ -20,6 +22,7 @@ class CatViewModel @Inject constructor(private val catRepository: CatRepository)
     val catState: StateFlow<List<Cat>> = _catState
 
     fun fetchCats() {
+        val time = measureTimeMillis {
         log("fetchCats() called from UI: main thread")
         viewModelScope.launch {
             log("fetchCats() inside viewModelScope: main thread")
@@ -27,11 +30,20 @@ class CatViewModel @Inject constructor(private val catRepository: CatRepository)
                 .catch { exception ->
                     Log.e("CatViewModel", exception.message, exception)
                 }
+                .buffer() //buffer emissions don't wait
                 //collect is called on main thread
                 .collect { cats ->
                     log("collect called on flow : main thread")
                     _catState.value += cats
                 }
+            }
         }
+        log("Collected in $time")
     }
 }
+/*
+milliseconds without buffer: 8
+milliseconds with buffer: 1
+We can use a buffer operator on a flow to run emitting code of the simple flow concurrently with collecting code,
+as opposed to running them sequentially
+ */
